22247cd59b6eff95cbc435aa015c9f8f6994cdd9
Juuso Metsävuori <juuso.metsavuori@gmail.com>
2017-08-26 19:10:41 +0300

    added a example command with set/get values

---
+++ commands/radio.py
@@ -0,0 +1,13 @@
+import logging
+
+class radio():
+   def __init__(self):
+      self.volume = 0
+
+   def set(self, args):
+      self.volume = int(args[2])
+      logging.info("cranked the radio to %s", self.volume)
+      return ''
+
+   def get(self, args):
+      return str(self.volume)
--- ship.py
+++ ship.py
@@ -1,15 +1,41 @@
-#!/usr/bin/python3
 import numpy as np
 from pyquaternion import Quaternion
+
+#game modules
+import commands.radio
 
 class ship():
    position = np.array([0,0,0])
    velocity = np.array([0,0,0])
 
    def __init__(self, position):
-      self.commands = {'echo':self.echo}
+      self.radio = commands.radio()
+
+      #set functionality
+      self.setCommands ={
+         'radio':self.radio.set
+      }
+
+      #get functionality
+      self.getCommands ={
+         'radio':self.radio.get
+      }
+
+      #basic commands
+      self.commands = {
+         'echo':self.echo,
+         'set':self.setCommand,
+         'get':self.getCommand
+      }
+
       self.position = np.array(position)
       self.velocity = np.array(0)
+
+   def setCommand(self, data):
+      return self.setCommands[data[1]](data)
+
+   def getCommand(self, data):
+      return self.getCommands[data[1]](data)
 
    def simulate(self, dt):
       
