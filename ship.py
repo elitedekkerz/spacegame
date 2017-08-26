127dc2a74491e3d6bd0ad3331129fc0c65ebe1f3
Mika <zidit@localhost.localdomain>
2017-08-26 19:03:06 +0300

    Added thruster simulation to the ship.

--- ship.py
+++ ship.py
@@ -5,20 +5,60 @@ import numpy as np
 from pyquaternion import Quaternion
 
 class ship():
-   position = np.array([0,0,0])
-   velocity = np.array([0,0,0])
+   ##Ships physical properties
+   #Mass in kg
+   mass = 1000000.0
+   #Thruster powers in N
+   thruster_power_front = 100000.0
+   thruster_power_back = 1000000.0
+
+   #Ships current control settings
+   thruster_front = 0.0
+   thruster_back = 0.0
+
+   #Ships position
+   position = np.array([0.0, 0.0, 0.0])
+   #Vector for telling where the ship is facing
+   front = np.array([0.0, 0.0, 0.0])
+   #Vector for telling  where ship is going
+   velocity = np.array([0.0, 0.0, 0.0])
+
+   def __init__(self):
+      __init__(position)
 
    def __init__(self, position):
-      self.commands = {'echo':self.echo}
+      self.commands = {
+         'echo':self.echo,
+         'thrust':self.thrust,
+         'fthrust':self.fthrust
+      }
+
       self.position = np.array(position)
-      self.velocity = np.array(0)
 
    def simulate(self, dt):
-      
-      pass
+      acceleration = np.array([0.0, 0.0, 0.0])
+      acceleration[0] = (self.thruster_back * self.thruster_power_back - self.thruster_front * self.thruster_power_front) / self.mass
+      self.velocity += acceleration * dt
+      self.position += self.velocity * dt
 
    def echo(self,data):
       return str.join(' ', data)
+
+   def thrust(self,data):
+      try:
+         val = float(data[0])
+         self.thruster_back = np.clip(val, 0, 1)
+         return "Back thruster set to: {0}".format(self.thruster_back)
+      except:
+         return "Error. Usage: thrust {float = 0.0 - 1.0}"
+   
+   def fthrust(self,data):
+      try:
+         val = float(data[0])
+         self.thruster_front = np.clip(val, 0, 1)
+         return "Front thruster set to: {0}".format(self.thruster_front)
+      except:
+         return "Error. Usage: fthrust {float = 0.0 - 1.0}"
 
 class player():
    #player as a class which can send user input commands to a ship
