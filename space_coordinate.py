2290611c9752966b63f7c27fb0411fcede9c8c83
Zidit <mika.terhokoski@gmail.com>
2017-09-12 20:20:56 +0300

    Fix space coordiante class

--- space_coordinate.py
+++ space_coordinate.py
@@ -8,12 +8,10 @@ class space_coordinate():
 
    position = np.array([0, 0, 0], dtype=np.int64)
 
-   def __init__(self):
-      __init__([0, 0, 0])
+   def __init__(self, position = [0, 0, 0]):
+      tmp = np.array(position) * 1000
+      self.position = tmp.astype(np.int64) 
 
-   def __init__(self, position):
-      position = np.array(position, dtype=np.int64)
-   
    def __repr__(self):
       return repr(self.position / 1000.0)
    
@@ -22,36 +20,45 @@ class space_coordinate():
 
 
    def __add__(self, other):
+      tmp = space_coordinate()
       if isinstance(other, space_coordinate):
-         return self.position + other
+         tmp.position = self.position + other.position
       else:
-         return self.position + (other * 1000.0).astype(np.int64) 
+         tmp.position = self.position + (other * 1000.0).astype(np.int64) 
+      return tmp
 
    def __sub__(self, other):
+      tmp = space_coordinate()
       if isinstance(other, space_coordinate):
-         return self.position - other
+         tmp.position = self.position - other.position
       else:
-         return self.position - (other * 1000.0).astype(np.int64) 
+         tmp.position = self.position - (other.position * 1000.0).astype(np.int64) 
+      return tmp
 
    def __radd__(self, other):  
       return __add__(self, other)
 
    def __rsub__(self, other):
+      tmp = space_coordinate()
       if isinstance(other, space_coordinate):
-         return other - self.position
+         tmp.position = other.position - self.position
       else:
-         return (other * 1000.0).astype(np.int64) - self.other
+         tmp.position = (other.position * 1000.0).astype(np.int64) - self.position
+      return tmp
 
    def __iadd__(self, other):
       if isinstance(other, space_coordinate):
-         self.position = self.position + other
+         self.position = self.position + other.position
       else:
-         self.position = self.position + (other * 1000.0).astype(np.int64) 
+         self.position = self.position + (other.position * 1000.0).astype(np.int64) 
       return self
 
    def __isub__(self, other):
       if isinstance(other, space_coordinate):
-         self.position = self.position - other
+         self.position = self.position - other.position
       else:
-         self.position = self.position - (other * 1000.0).astype(np.int64) 
+         self.position = self.position - (other.position * 1000.0).astype(np.int64) 
       return self
+
+   def __abs__(self):
+      return np.linalg.norm(self.position) / 1000
