236aca618f11b881c7055ac1dbfd51730ad184c3
Zidit <mika.terhokoski@gmail.com>
2017-09-12 22:15:55 +0300

    Improve radar scan

--- commands/radar.py
+++ commands/radar.py
@@ -26,9 +26,19 @@ class radar():
       pass
 
    def scan(self, args):
-      result = ""
+      found_objects = []
 
       for obj in gameObject.objects:
          if obj.getDistanceTo(self.ship) < self.range: 
-            result += str(obj) + "\n"
+            if obj != self.ship:
+               dPos = obj.position - self.ship.position
+               a = (obj.identifier, abs(dPos), dPos)
+               found_objects.append(a) 
+
+      found_objects.sort(key=lambda tup: tup[1])
+
+      result = ""
+      for i in found_objects:
+         result += str(i) + "\n"
+
       return result
