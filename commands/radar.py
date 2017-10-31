d1c098b3416f184721b53e07b399a59234a83489
Zidit <mika.terhokoski@gmail.com>
2017-10-31 20:15:20 +0200

    Change radar to do scan in simulation every 2 seconds
    When radar is on the simulation will do the scan in background every
    2 seconds. The users "scan" command now only need to return the prescanned
    list.
    
    This helps to limit the users scan spam and limits the runs of the real
    scan that is bit heavy.

--- commands/radar.py
+++ commands/radar.py
@@ -17,6 +17,10 @@ class radar():
       self.power_consumption = 1000000
       self.powered = False
       self.power_factor = 1
+      self.next_scan = 0
+
+      #keep list of the objects that are in scan range
+      self.objects_in_range = []
 
    def parse(self, args):
       commands = {
@@ -36,25 +40,36 @@ class radar():
 
    def simulate(self, dt, power_factor):
       self.power_factor = power_factor
+      
+      #Update scan every 2 seconds
+      self.next_scan -= dt
+      if self.next_scan < 0 and self.powered:
+         self.sim_scan()
+         self.next_scan = 2
+
+   def sim_scan(self):
+      self.objects_in_range.clear()
+
+      #Get scan range
+      eff_range = self.range * (180 / self.sector) * self.power_factor
+
+      #Find all object that are in the scan cone
+      for obj in gameObject.objects:
+         if self.isInRange(obj, eff_range):
+            if obj != self.ship:
+               self.objects_in_range.append(obj) 
+
+      #Sort found objects by distance
+      self.objects_in_range.sort(key=lambda tup: tup.getDistanceTo(self.ship))
+
 
    def scan(self, args):
       if not self.powered:
          return 'Error', "Radar is turned off"
-
-      found_objects = []
-
-     # eff_range = self.range * (180 / self.sector) * self.power_factor
-
-      for obj in gameObject.objects:
-      #   if obj.getDistanceTo(self.ship) < eff_range and self.ship.getAngleTo(obj, True) < self.sector: 
-         if self.isInRange(obj):
-            if obj != self.ship:
-               found_objects.append(obj) 
-
-      found_objects.sort(key=lambda tup: tup.getDistanceTo(self.ship))
-
+      
+      #Generate string from the scaned objects
       result = ""
-      for i in found_objects:
+      for i in self.objects_in_range:
          dist, azimuth, inclination = self.ship.getSphericalCoordinateTo(i, inDeg = True)
          result += "{}: {:06.3E} {:+06.1f} {:+06.1f}\n".format(i.identifier, dist, azimuth, inclination)
 
@@ -72,6 +87,7 @@ class radar():
 
    def powerOn(self, args):
       self.powered = True
+      self.next_scan = 0
       return 'Ok', " "
    
    def powerOff(self, args):
@@ -84,8 +100,7 @@ class radar():
       else:
          return 0
 
-   def isInRange(self, obj):
-      eff_range = self.range * (180 / self.sector) * self.power_factor
+   def isInRange(self, obj, eff_range):
       return obj.getDistanceTo(self.ship) < eff_range and self.ship.getAngleTo(obj, True) < self.sector
 
    def identify(self, args):
@@ -93,10 +108,11 @@ class radar():
          return 'Error', "Radar is turned off"
 
       target = None
+      eff_range = self.range * (180 / self.sector) * self.power_factor
 
       #find target from game objects
       for obj in gameObject.objects:
-         if self.isInRange(obj) and obj.identifier == args[2]: 
+         if obj.identifier == args[2] and self.isInRange(obj, eff_range): 
             target = obj
             break
       
