bccbf19b7e0cdc865f43e9f0b0f5ad5dcd613cc7
Juuso Metsävuori <juuso.metsavuori@gmail.com>
2018-01-02 22:14:47 +0200

    added future features to player

--- player.py
+++ player.py
@@ -1,25 +1,49 @@
 from types import *
 import logging
-import ship
-
-logger = logging.getLogger('player')
 
 class player():
-   #player as a class which can send user input commands to a ship
-   name = 'Yuri'
+    '''process data received from client'''
+    def __init__(self, client)
+        #functionality variables
+        self.client = client
+        self.log = logging.getLogger(self.client.addressString)
+        #game variables
+        self.name = 'Yuri'
+        self.ship = None
+        self.log.info('ready')
 
-   #string for collecting command
-   inputString = ''
-   def readInput(self, data):
-      assert type(data) is str, 'data is not a string %s' % data
-      for char in data:
-         #command complete?
-         if char == '\n':
-            args = self.inputString.split()
-            self.inputString = ''
-            logger.debug("constructed command %s", str(args))
-            return args
+    def disconnect(self):
+        '''remove self from ship and kill client'''
+        self.leaveShip()
+        self.alive = False
+        self.log.info('disconnected')
 
-         else:
-            self.inputString += char
-      return {}
+    def setName(self, name):
+        '''change onscreen name'''
+        self.name = name
+        self.log.info('changed name to %s', self.name)
+
+    def joinShip(self, ship):
+        '''add self to ship'''
+        self.leaveShip()
+        self.ship = ship
+        self.ship.crew.append(self)
+        self.log.info('joined %s', self.ship.name)
+
+    def leaveShip(self):
+        '''remove self from current ship'''
+        if self.ship:
+            self.ship.crew.remove(self)
+        self.ship = None
+
+    def echo(self, message):
+        '''return what was given'''
+        return message
+
+    def parse(self):
+        '''process a command received from the client'''
+        #echo as placeholder
+        message = self.client.getMessage()
+        if message:
+            reply = self.echo(message)
+            self.client.sendMessage(reply)
